ad19d09881efe112b16e6a296647ffdc45010425
Gerasimova Anastasia <ger.an.a@yandex.ru>
2022-11-16 13:25:27 +0300

    Сверстать базовую версию

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ import Manuals from './Manuals';
 //import Events from './Events';
 import TextSection from './TextSection';
 import Product from './Product';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
@@ -15,34 +15,32 @@ function App() {
       <div className="page">
         <BrowserRouter>
           <Header />
-          <Switch>
-            <Route exact path="/">
-              <Redirect to="/main" />
-            </Route>
-            <Route path="/main">
-              <Main />
-            </Route>
-            <Route exact path="/catalog">
-              <Catalog />
-            </Route>
-            <Route path={`/catalog/:level`}>
-              <Product></Product>
-            </Route>
-            <Route path="/manuals">
-              <Manuals />
-            </Route>
-            <Route path="/contacts">
-              <TextSection class="section" level="contacts" page="contacts" />
-            </Route>
-            <Route path="/about">
-              <TextSection class="section" level="about" page="about" />
-            </Route>
-            {/*
+          <Route exact path="/">
+            <Redirect to="/main" />
+          </Route>
+          <Route path="/main">
+            <Main />
+          </Route>
+          <Route exact path="/catalog">
+            <Catalog />
+          </Route>
+          <Route path={`/catalog/:level`}>
+            <Product></Product>
+          </Route>
+          <Route path="/manuals">
+            <Manuals />
+          </Route>
+          <Route path="/contacts">
+            <TextSection class="section" level="contacts" page="contacts" />
+          </Route>
+          <Route path="/about">
+            <TextSection class="section" level="about" page="about" />
+          </Route>
+          {/*
           <Route path="/events">
             <Events />
           </Route> 
           */}
-          </Switch>
           <Footer />
           <div className="triangle"></div>
         </BrowserRouter>
