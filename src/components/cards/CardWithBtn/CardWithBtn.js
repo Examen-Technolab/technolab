7127ff9b61247431febf82d20b6b8d8ace5a664d
ger.an.a <ger.an.a@yandex.ru>
2024-08-12 21:19:35 +0300

    styles

--- src/components/cards/CardWithBtn/CardWithBtn.js
+++ src/components/cards/CardWithBtn/CardWithBtn.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 import { Card } from '../Card/Card';
 
+import style from './CardWithBtn.module.css'
+
 function CardWithBtn(props) {
   return (
     <Card type="with-btn" card={props.card}
       children={
-        <Link to={`/catalog/${props.card.id}/kit`} className={`card__link-btn card__link-btn_level_${props.card.type} card-with-btn__btn`}>Узнать больше</Link>
+        <Link to={`/catalog/${props.card.id}/kit`} className={`card__link-btn card__link-btn_level_${props.card.type} ${style.btn}`}>Узнать больше</Link>
       }
     />
   );
